5d193a7c3c5b98c9ff0ba0fab83fe54e1389085b
Kundan547 <kundanvyas197@gmail.com>
2025-01-26 11:03:38 +0530

    Added skils i know and perform split() method on them

--- str1.py
+++ str1.py
@@ -1,3 +1,12 @@
 sentance = "Python is general purpose, High level, dynamic typed, object priendted programing lanugage."
 word = sentance.split()
 print(word)
+
+#Other example of it.
+
+Data = "Docker, Kuberenetes, Jenkins, Aws, Grafana, Gitlabs, Git , Github, Python, Java"
+
+#Here we have different types of objects for that.
+
+skils = Data.split(",",2)
+print(skils)
