af483262f6cc96e451ae7313483ba18d7eab4154
Kundan547 <kundanvyas197@gmail.com>
2025-01-26 21:39:56 +0530

    A function for test function.

---
+++ full_names.py.py
@@ -0,0 +1,7 @@
+def get_full_name(first, last):
+    """Return a full name"""
+    full_name = "{0} {1}".format(first, last)
+    return full_name.title()
+
+name = get_full_name('Kunal', 'Joshi') 
+print(name)  
