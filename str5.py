a52ed2f9c3d4243321bb67b27e00ed26dc19e92a
Kundan547 <kundanvyas197@gmail.com>
2025-02-01 23:13:31 +0530

    Anagram program for strings.

---
+++ str5.py
@@ -0,0 +1,8 @@
+a=input()
+b=input()
+def anagram(a,b):
+  for i in a:
+    if a.count(i)==b.count(i):
+      return print(f'{a} and {b} are anagram')
+    return print(f'{a} and {b} are not anagram')
+anagram(a,b)
