0ea705b2845b00831829d758a2e2f7db6c575df3
Kundan547 <kundanvyas197@gmail.com>
2025-02-01 14:53:43 +0530

    Added new program.

---
+++ str2.py
@@ -0,0 +1,11 @@
+alphabets = ['a','b','c','d']
+for items in alphabets:
+	print(len(items))
+
+
+#Other methods
+alphabets = ['a', 'b', 'c'] 
+fn = len
+for item in alphabets: 
+        print(fn(item))
+        
