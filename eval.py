8dd3b596fad4af2f4904c7641af9c8a419173530
Kundan547 <kundanvyas197@gmail.com>
2025-02-02 12:54:49 +0530

    eval function works on both integer and string.

--- eval.py
+++ eval.py
@@ -1,7 +1,10 @@
 expression = 'x*(x+1)*(x+2)*(x+3)'
+stringexpression = 'x1+x1'
 print(expression)
 
 x = 3
+x1 = 'Devops'
 
 result = eval(expression)
+result = eval(stringexpression)
 print(result)
