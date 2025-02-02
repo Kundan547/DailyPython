a6651999fc1344652416a32886632bc21d8aeabd
Kundan547 <kundanvyas197@gmail.com>
2025-02-02 10:43:21 +0530

    Modify the function

--- str5.py
+++ str5.py
@@ -4,5 +4,7 @@ def anagram(a,b):
   for i in a:
     if a.count(i)==b.count(i):
       return print(f'{a} and {b} are anagram')
-    return print(f'{a} and {b} are not anagram')
+    else:
+        return print(f'{a} and {b} are not anagram')
+    
 anagram(a,b)
