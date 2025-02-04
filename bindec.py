7ee719688f83c1a32aedb55e718e34125c6256bd
Kundan547 <kundanvyas197@gmail.com>
2025-02-04 12:36:40 +0530

    BInary to desimal

---
+++ bindec.py
@@ -0,0 +1,13 @@
+def convert_to_binary(num):
+    if num == 0:
+        return "0"
+    binary = []
+    while num > 0:
+        binary.append(str(num % 2))
+        num //= 2
+    binary.reverse()
+    return ''.join(binary)
+
+n = int(input("Enter a number: "))
+binary_representation = convert_to_binary(n)
+print(f"Binary representation of {n} is: {binary_representation}")
