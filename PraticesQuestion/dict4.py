a1550ab83dfb5ba3498a31c8b01f8355848a26d6
Kundan547 <kundanvyas197@gmail.com>
2025-02-15 18:35:06 +0530

    Key:value in list.

---
+++ PraticesQuestion/dict4.py
@@ -0,0 +1,4 @@
+list_of_dicts = [{'name': 'Kundan', 'age': 22}, {'name': 'Kuldeep', 'age': 20}]
+for d in list_of_dicts:
+    for key, value in d.items():
+        print(f"Key: {key}, Value: {value}")
