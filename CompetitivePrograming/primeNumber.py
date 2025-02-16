636a4ce13c74cca22d8d355b2d9a38be79e76816
Kundan547 <kundanvyas197@gmail.com>
2025-02-16 12:27:27 +0530

    fix the fix and code is working properly.

--- CompetitivePrograming/primeNumber.py
+++ CompetitivePrograming/primeNumber.py
@@ -4,13 +4,16 @@ def genprimes(n):
     primes[0] = False
     primes[1] = False
     for p in range(2,int(sqrt(n))+1):
+        
         if primes[p] == True:
+            
             for i in range(p*p, n+1, p):
                 primes[i] = False
                 
                 
     
     for i in range(0, len(primes)):
+        
         if primes[i] == True:
             print(i, end=" ")
             
