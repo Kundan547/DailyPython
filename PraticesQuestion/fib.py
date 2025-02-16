7d5f61342043f249a427ac2d8347e8b3a35ace04
Kundan547 <kundanvyas197@gmail.com>
2025-02-16 21:51:07 +0530

    The fibonachi series by using recursive method.

---
+++ PraticesQuestion/fib.py
@@ -0,0 +1,12 @@
+def fib(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return fib(num - 1) + fib(num - 2)
+    
+n = int(input("Enter a number to generate the fibonachi series: "))
+print("Fibonachi series is: ")
+for i in range(0, n):
+    print(fib(i))
