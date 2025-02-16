b6a7128a6d6ecc71e38e83abdbace7a10f283b4a
Kundan547 <kundanvyas197@gmail.com>
2025-02-16 16:47:35 +0530

    Fixed the issue

--- CompetitivePrograming/bitmagic.py
+++ CompetitivePrograming/bitmagic.py
@@ -5,17 +5,14 @@ def evenodd(n):
         print("even")
 
 def mulpow2(x,y):
-    
     return x << y
 
 def divpow2(x,y):
-    
     return x >> y
 
 t = int(input())
 while t:
     x,y = map(int,input().split())
-    
     print(mulpow2(x,y))
     print(divpow2(x,y))
     t=t-1
