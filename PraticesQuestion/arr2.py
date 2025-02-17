837acb919b20e7f552681e5235374e05592262e9
Kundan547 <kundanvyas197@gmail.com>
2025-02-17 18:05:09 +0530

    Finding the missing number in array.

---
+++ PraticesQuestion/arr2.py
@@ -0,0 +1,9 @@
+def find_missing_element(array):
+    expected_sum = sum(range(1,101))
+    actual_sum = sum(array)
+    return expected_sum - actual_sum
+
+array = [x for x in range(1,101) if x not in[3,4,6,7,9]]
+missing_number = find_missing_element(array)
+
+print("The missing number is: ", missing_number)
