526f3cb97dece4d0f9cc0ab98eee1da16307e8c9
Kundan547 <kundanvyas197@gmail.com>
2025-02-17 22:07:51 +0530

    Bug is fixed.

--- PraticesQuestion/arr2.py
+++ PraticesQuestion/arr2.py
@@ -4,6 +4,7 @@ def find_missing_element(array):
     return expected_sum - actual_sum
 
 array = [x for x in range(1,101) if x not in[3,4,6,7,9]]
+
 missing_number = find_missing_element(array)
 
 print("The missing number is: ", missing_number)
