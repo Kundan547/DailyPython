05421bcc274d9d7614b9544a618a9915d10aad70
Kundan547 <kundanvyas197@gmail.com>
2025-02-17 10:54:24 +0530

    GCD of two words using recursion.

---
+++ PraticesQuestion/gcp.py
@@ -0,0 +1,9 @@
+def gcp(num1, num2):
+    if num2 == 0:
+        return num1;
+    return gcp(num2, num1 % num2)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(gcp(num1, num2))
