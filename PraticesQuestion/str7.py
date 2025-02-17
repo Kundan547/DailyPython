1f8ed7f7901df07a324539c89b921ddd2e2cca27
Kundan547 <kundanvyas197@gmail.com>
2025-02-17 12:36:20 +0530

    ANagram for strings.

---
+++ PraticesQuestion/str7.py
@@ -0,0 +1,12 @@
+def anangram(str1, str2):
+    if (sorted(str1) == sorted(str2)):
+        return True
+    else:
+        return False
+    
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+if anangram(str1, str2):
+    print("Anangram: ")
+else:
+    print("Not Anangram")
