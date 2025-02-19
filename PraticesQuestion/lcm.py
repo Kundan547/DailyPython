5411f87ee2010854cbeca9ee081a0490e15cc748
Kundan547 <kundanvyas197@gmail.com>
2025-02-19 13:26:28 +0530

    Completed.

--- PraticesQuestion/lcm.py
+++ PraticesQuestion/lcm.py
@@ -9,6 +9,5 @@ else:
     
 for i in range(1, minimum+1):
     if((num1 % i == 0) and (num2 % i == 0 )):
-        hcf = i
-        
+        hcf = i        
 print("hcf/gcd of", num1, "and", num2, "=", hcf)
