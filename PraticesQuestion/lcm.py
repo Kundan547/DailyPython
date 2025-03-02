7f949a8d1562fdc138319d5f5588276fad748fa0
Kundan547 <kundanvyas197@gmail.com>
2025-03-02 23:20:37 +0530

    Fixed the bug and completed.

--- PraticesQuestion/lcm.py
+++ PraticesQuestion/lcm.py
@@ -1,13 +1,17 @@
-num1 = int(input("Enter frist number: "))
-num2 = int(input("Enter second number: "))
+def calculate_hcf(x, y):
+    while(y):
+        x, y = y, x % y
+    return x
 
-if num1 > num2:
-    minimum = num2
-    
-else:
-    minimum = num1
-    
-for i in range(1, minimum+1):
-    if((num1 % i == 0) and (num2 % i == 0 )):
-        hcf = i        
-print("hcf/gcd of", num1, "and", num2, "=", hcf)
+def calculate_lcm(x, y):
+    hcf = calculate_hcf(x, y)
+    return (x * y) // hcf
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    lcm = calculate_lcm(num1, num2)
+    print("LCM of", num1, "and", num2, "=", lcm)
+except ValueError:
+    print("Please enter valid integers.")
