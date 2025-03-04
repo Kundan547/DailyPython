1c6a9167f2f4bfae16f49743e45d6a010d3246ab
Kundan547 <kundanvyas197@gmail.com>
2025-03-04 14:44:50 +0530

    Fixed the bug and completed.

--- PraticesQuestion/point.py
+++ PraticesQuestion/point.py
@@ -1,19 +1,15 @@
 from enum import Enum
-
 class Color(Enum):
     RED = 'red'
     GREEN = 'green'
     BLUE = 'blue'
 
-try:
-    color = Color(input("Enter your choice of 'red', 'blue' or 'green': ").strip().lower())
-    
-    match color:
-        case Color.RED:
-            print("I see red!")
-        case Color.GREEN:
-            print("Grass is green")
-        case Color.BLUE:
-            print("I'm feeling the blues :(")
-except ValueError:
-    print("Invalid color choice. Please enter 'red', 'blue', or 'green'.")
+color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
